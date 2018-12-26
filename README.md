ca162cc7f6cdb4ca7b183f167f84e9db39532d51
ahmed samir <ahmed.samir.farid@gmail.com>
2018-12-26 19:34:31 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# March
+# March Template Version: 1.0
+## Depends on  Bootstrap Library
+This Template For Freelancers To Define Themselves And Display Their Works.
+
+
+Download PSD: [PSD File](https://github.com/ahmedfaheem/Glazzy-Theme/blob/master/psd%20file.zip).
+
+Review : [Glazzy Demo](https://ahmedfaheem.github.io/Glazzy-Theme/)
+
+![Glazzy Image](https://github.com/ahmedfaheem/Glazzy-Theme/blob/master/glazzy-image.png)
+
+##  Photos of Site Pages
+![About Image](https://github.com/ahmedfaheem/Glazzy-Theme/blob/master/images/Photos-of-Site-Pages/about.png)
+![Work Image](https://github.com/ahmedfaheem/Glazzy-Theme/blob/master/images/Photos-of-Site-Pages/work.png)
+![Labs Image](https://github.com/ahmedfaheem/Glazzy-Theme/blob/master/images/Photos-of-Site-Pages/labs.png)
+![Contact Us Image](https://github.com/ahmedfaheem/Glazzy-Theme/blob/master/images/Photos-of-Site-Pages/contact.png)
+
+## Content:
+1. CSS Foder
+     - Font Awesome > all.min.css
+     - Animate      > animate.min.css
+     - Bootstrap    > bootstrap.css
+     - Style        > style.css
+    
+2. Template Images > All Images and illustrations are just for demonstration, all right reserved by Vijay Verma.
+
+3. Js Foder
+     - Bootstrap  > bootstrap.min.js	 bootstrap.min. V 4.0.0
+     - jquery     > jquery-3.3.1.min.js	
+     - plugin     > plugin.js	  
+     - popper     > popper.min.js
+     - wow        > wow.min.js
+     
+3. Web Fonts 
+
+4. License
+
+5. index.html
+
+6. logo.ico    
+    
+ ## Languages and libraries  
+ - Html
+ - Css
+ - Bootstrap
+ - jquery
+ - Wow js
+ - magnific popup js
+ 
+    
+
+### License
+
+[MIT](https://github.com/ahmedfaheem/Glazzy-Theme/blob/master/License).
+
+### Support or Contact
+ Copyright (c) 2018 Ahmed samir  ahmed.samir.farid@gmail.com github.com/a7medsamirf
+ 
+ ### [Facebook Account](https://www.facebook.com/a7med.samir.f)
+
---
+++ assets/js/custom.js
@@ -0,0 +1,140 @@
+/*global $ , alert, console*/
+
+$(document).ready(function() {
+  $('body').bootstrapMaterialDesign();
+});
+
+  // Start niceScroll
+  $(document).ready(function () {
+    $("html").niceScroll({
+      scrollspeed: "100",
+      cursorcolor: "#FB3B64",
+      cursorborder: "3px solid #FB3B64",
+      cursorborderradius:0,
+      sensitiverail: false,
+  });
+}); 
+     //Check the scroll position
+$(window).scroll(function(){
+	if ($(this).scrollTop() > 100) {
+		event.preventDefault();
+		$(".scrollToTop").css({'transform': 'translate(0px, 0px)'});
+	} else {
+		$(".scrollToTop").css({'transform': 'translate(80px, 0px)'});
+	}
+});
+
+
+//Click and scrolls top effect
+$('.scrollToTop').click(function(){
+  $('html, body').animate({scrollTop : 0},800);
+  return false;
+  });
+
+
+$(document).ready(function () {
+new WOW().init();
+}); 
+
+$(document).ready(function () {
+    var scroll = new SmoothScroll('a[href*="#"]');
+    }); 
+    
+
+// Changing nav-bar color after scrolling
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+  
+    
+
+    // showcase slider
+ $(document).ready(function (){
+function moveToSelected(element) {
+
+  if (element == "next") {
+    var selected = $(".selected").next();
+  } else if (element == "prev") {
+    var selected = $(".selected").prev();
+  } else {
+    var selected = element;
+  }
+
+  var next = $(selected).next();
+  var prev = $(selected).prev();
+  var prevSecond = $(prev).prev();
+  var nextSecond = $(next).next();
+
+
+  $(selected).removeClass().addClass("selected");
+
+  $(prev).removeClass().addClass("prev");
+  $(next).removeClass().addClass("next");
+
+  $(nextSecond).removeClass().addClass("nextRightSecond");
+  $(prevSecond).removeClass().addClass("prevLeftSecond");
+
+  $(nextSecond).nextAll().removeClass().addClass('hideRight');
+  $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+
+}
+
+// Eventos teclado
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        moveToSelected('prev');
+        break;
+
+        case 39: // right
+        moveToSelected('next');
+        break;
+
+        default: return;
+    }
+    e.preventDefault();
+});
+
+$('.showcase div').click(function() {
+  moveToSelected($(this));
+});
+
+$('#prev').click(function() {
+  moveToSelected('prev');
+});
+
+$('#next').click(function() {
+  moveToSelected('next');
+});
+});
+
+
+
+// Loading
+$(window).on('load', function(){
+
+	$('.sk-cube-grid').delay(0).fadeOut(1000, function(){
+		$('.preload').slideUp(1000);
+	});	
+
+});
+
+
+// popup
+$(document).ready(function() {
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false
+  });
+});
+
+
+
